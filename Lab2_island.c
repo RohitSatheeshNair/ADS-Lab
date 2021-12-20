81a7f05a777f5e0a28a8674e0b16c77ba3537b8b
RohitSatheeshNair <71492948+RohitSatheeshNair@users.noreply.github.com>
2021-12-20 21:34:28 +0530

    Update Lab2_island.c

--- Lab2_island.c
+++ Lab2_island.c
@@ -1,63 +1,114 @@
-#include<stdio.h>
+#include <bits/stdc++.h>
 
-int check_safe(int n,int m,int i, int j)
+using namespace std;
+
+class DisjointSet 
 {
-    if(i<0||i>=n)
-     return 0;
-    if(j<0||j>=m)
-     return 0;
-    return 1;
-}
+	vector<int> rank,parent;
+	int n;
+	
+	public:
+		
+	DisjointSet(int n) {
+		rank.resize(n);
+		parent.resize(n);
+		this->n = n;
+		makeSet();
+	}
+		
+	void makeSet() {
+		for (int i=0 ;  i<n ; i++) {
+			parent[i] = i;	
+		}		
+	}
+	
+	int find(int x) {
+		if(parent[x] != x) {
+			parent[x] = find(parent[x]);
+		}
+		return parent[x];
+	}
+	
+	void Union(int x,int y) {
+		int xRoot = find(x);
+		int yRoot = find(y);
+		
+		if(xRoot == yRoot) {
+			return;
+		}
+		
+		if(rank[xRoot] < rank[yRoot]) {
+			parent[xRoot] = yRoot;
+		}
+		else if(rank[yRoot] < rank[xRoot]) {
+			parent[yRoot] = xRoot;
+		}
+		else {
+			parent[yRoot] = xRoot;
+			rank[xRoot]++;
+		}
+	}
+};
 
-void island(int I[][50],int n, int m, int i, int j)
+int countIslands(vector<vector<int>>a) 
 {
-    if(check_safe(n,m,i,j)&&I[i][j]==1)
-    {
-        I[i][j]=0;
-        island(I,n,m,i-1,j-1);
-        island(I,n,m,i-1,j);
-        island(I,n,m,i-1,j+1);
-        island(I,n,m,i,j-1);
-        island(I,n,m,i,j+1);
-        island(I,n,m,i+1,j-1);
-        island(I,n,m,i+1,j);
-        island(I,n,m,i+1,j+1);
-    }
-}
-
-int no_of_islands(int I[][50],int n, int m) 
-{
-    int count=0;
-    int i,j;
-    for(i=0;i<n;i++)
-    {
-        for(j=0;j<m;j++)
-         if(I[i][j]==1)
-         {
-             count++;
-             island(I,n,m,i,j);
-         }
-    }
-if(count==0)
- return 0;
-return count;
-}
-
-
-int main()
-{
-    int i,j,n,m;
-    int I[50][50];
-    printf("Enter the number of rows and columns\n");
-    scanf("%d %d",&n,&m);
-    printf("Enter the values:\n");
-    for(i=0;i<n;i++)
-    {
-        for(j=0;j<m;j++)
-        {
-            scanf("%d",&I[i][j]);
+	int n = a.size();
+	int m = a[0].size();
+	
+	DisjointSet *dis = new DisjointSet(n*m);
+	
+	for (int j=0 ; j<n ; j++) {
+		for (int k=0 ; k<m ; k++) {
+			if (a[j][k] == 0)
+                continue;
+            if (j + 1 < n && a[j + 1][k] == 1)
+                dis->Union(j * (m) + k,(j + 1) * (m) + k);
+            if (j - 1 >= 0 && a[j - 1][k] == 1)
+                dis->Union(j * (m) + k,(j - 1) * (m) + k);
+            if (k + 1 < m && a[j][k + 1] == 1)
+                dis->Union(j * (m) + k,(j) * (m) + k + 1);
+            if (k - 1 >= 0 && a[j][k - 1] == 1)
+                dis->Union(j * (m) + k,(j) * (m) + k - 1);
+            if (j + 1 < n && k + 1 < m && a[j + 1][k + 1] == 1)
+                dis->Union(j * (m) + k,(j + 1) * (m) + k + 1);
+            if (j + 1 < n && k - 1 >= 0 && a[j + 1][k - 1] == 1)
+                dis->Union(j * m + k,(j + 1) * (m) + k - 1);
+            if (j - 1 >= 0 && k + 1 < m && a[j - 1][k + 1] == 1)
+                dis->Union(j * m + k,(j - 1) * m + k + 1);
+            if (j - 1 >= 0 && k - 1 >= 0 && a[j - 1][k - 1] == 1)
+                dis->Union(j * m + k,(j - 1) * m + k - 1);
+		}
+	}
+	
+    int *c = new int[n * m];
+    int num_islands = 0;
+    
+    for (int j=0; j<n; j++) {
+        for (int k=0; k<m; k++) {
+            
+            if (a[j][k] == 1) {
+                int x = dis->find(j * m + k);
+                
+                if (c[x] == 0) {
+                    num_islands++;
+                    c[x]++;
+                }
+                else {
+                    c[x]++;
+                }            
+            }
         }
     }
-   printf("\nTotal number of islands : %d", no_of_islands(I,n,m));
-    return 0;
+    
+    return num_islands; 	
+}
+
+int main(void)
+{
+    vector<vector<int>>A = {{1, 1, 0, 0, 0},
+                            {0, 1, 0, 0, 1},
+                            {1, 0, 0, 1, 1},
+                            {0, 0, 0, 0, 0},
+                            {1, 0, 1, 0, 1}};
+    cout << "Number of Islands is: " << countIslands(A) << endl;
 }
